8d5d9f491dcb401fff2978c533f301eafcb52b75
ninjalog27 <renzhe.cs@outlook.com>
2026-07-14 18:09:24 +0800

    Site updated: 2026-07-14 18:09:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/28/hello-world/","2026/03/28/我的第一篇笔记/","2026/03/28/个人站搭建流程/"];function toRandomPost(){
+var posts=["2026/03/28/hello-world/","2026/03/28/个人站搭建流程/","2026/03/28/我的第一篇笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
